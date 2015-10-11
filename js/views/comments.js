8754a0a691c05a7bf7bfa073b2549bf861609ba8
Francisco De La Cruz <helloquico@gmail.com>
2015-10-10 22:08:56 -0400

    Implement a View to display the Store's comments

--- js/views/comments.js
+++ js/views/comments.js
@@ -1,13 +1,45 @@
 var React = require('react');
 
+var CommentStore = require('../stores/comment-store');
+
+function getStateFromStore() {
+	return {
+		comments: CommentStore.getAll()
+	};
+}
+
 var Comments = React.createClass({
 
-  render: function() {
-    return (
-      <div />
-    );
-  }
+	onChange: function () {
+		this.setState(getStateFromStore());
+	},
 
+	// think of functions as values
+	getInitialState: getStateFromStore,
+
+	componentDidMount: function () {
+		CommentStore.addChangeListener(this.onChange);
+	},
+
+	componentWillMount: function () {
+		CommentStore.removeChangeListener(this.onChange);
+	},
+
+  render: function() {
+		var comments = this.state.comments.map(function (comment, index) {
+			return (
+					<div className='comment' key={'comment-' + index}>
+						{comment.text}
+					</div>
+			)
+		});
+
+		return (
+				<div className='comments'>
+					{comments}
+				</div>
+		);
+  }
 });
 
 module.exports = Comments;
